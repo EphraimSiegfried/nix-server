6d30cf628f462ad43f77fde5e7c531611075878e
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-06-09 12:08:23 +0200

    update openssh settings

--- nixos/system/ssh-server.nix
+++ nixos/system/ssh-server.nix
@@ -3,6 +3,11 @@
     enable = true;
     settings = {
       PasswordAuthentication = false;
+      PubkeyAuthentication = "yes";
+      PermitRootLogin = "no";
+      X11Forwarding = false;
+      AllowTCPForwarding = "no";
+      PermitTunnel = "no";
     };
   };
 }
