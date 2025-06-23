82b777ef3c97ffb20c76ce0f29d01fa0ef61149b
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-06-23 22:33:06 +0200

    add widgets and background to homepage

--- nixos/applications/services/homepage.nix
+++ nixos/applications/services/homepage.nix
@@ -4,6 +4,21 @@
     enable = true;
     listenPort = 8082;
     allowedHosts = config.domain;
+    settings = {
+      title = "Zenolium";
+      description = "Welcome to Zenolium";
+      background = {
+        image = "https://preview.redd.it/space-wallpapers-4k-3840-2160-v0-xlbdzh75m88f1.jpg?width=1080&crop=smart&auto=webp&s=325d1f2a3c59d23ecbbdf6ad10000299b11b94b9";
+        blur = "md";
+      };
+      layout = {
+        Media = {
+          style = "row";
+          columns = 5;
+
+        };
+      };
+    };
     services = [
       {
         "Media" = [
@@ -11,30 +26,49 @@
             "Jellyfin" = {
               href = "https://jelly.${config.domain}";
               icon = "jellyfin";
+              description = "Media server for streaming movies, shows, and music.";
             };
           }
           {
             "Jellyseerr" = {
               href = "https://js.${config.domain}";
               icon = "jellyseerr";
+              description = "Request interface for movies and shows in Jellyfin.";
             };
           }
           {
             "Radarr" = {
               href = "https://radarr.${config.domain}";
               icon = "radarr";
+              description = "Automated movie downloader and organizer.";
             };
           }
           {
             "Sonarr" = {
               href = "https://sonarr.${config.domain}";
               icon = "sonarr";
+              description = "Automated TV show downloader and organizer.";
             };
           }
           {
             "Prowlarr" = {
               href = "https://prowlarr.${config.domain}";
               icon = "prowlarr";
+              description = "Indexer manager for Radarr, Sonarr, and others.";
+            };
+          }
+        ];
+
+      }
+      {
+
+        "Cloud" = [
+          {
+            "Vaultwarden" = {
+              href = "https://vw.${config.domain}";
+              icon = "vaultwarden";
+              description = "Password manager compatible with Bitwarden clients";
+
             };
           }
         ];
@@ -46,12 +80,21 @@
             "Grafana" = {
               href = "https://grafana.${config.domain}";
               icon = "grafana";
+              description = "Dashboard tool for visualizing metrics and logs.";
             };
           }
         ];
-
       }
 
+    ];
+    widgets = [
+      {
+        resources = {
+          cpu = true;
+          disk = "/";
+          memory = true;
+        };
+      }
     ];
   };
   services.nginx.virtualHosts."${config.domain}" = {
