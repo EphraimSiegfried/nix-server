532115ed2df04bb552005df88b8177323036bc8f
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-22 08:33:49 +0200

    update flake

--- nixos/applications/services/nextcloud.nix
+++ nixos/applications/services/nextcloud.nix
@@ -5,7 +5,7 @@ in
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud31;
+    package = pkgs.nextcloud32;
     hostName = "${subdomain}.${config.domain}";
     home = "${config.data_dir}/nextcloud";
     maxUploadSize = "16G";
