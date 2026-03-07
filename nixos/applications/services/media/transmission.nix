6d6337739bc055334ff7775f490d29173eec19da
zenoli <olibitter@gmail.com>
2026-03-07 23:43:59 +0100

    add timeout to wg.service to avoid race condition

--- nixos/applications/services/media/transmission.nix
+++ nixos/applications/services/media/transmission.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   sops.secrets."wg-conf" = { };
   nixarr = {
@@ -16,5 +16,11 @@
       flood.enable = true;
       uiPort = 9091; # standard port for transmission
     };
+  };
+  systemd.services.wg.serviceConfig = {
+    # Hack to avoid a race condition where wg tried to read the wireguard entpoint 
+    # `nl3.vpn.airdns.org` before the network is up and running.
+    # This is not a proper fix but good enough for now.
+    ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
   };
 }
