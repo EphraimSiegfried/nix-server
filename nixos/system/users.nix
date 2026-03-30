900d6d9a3fb1fe5ada0dca7cb2761a9fd6a0e17d
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-03-30 16:00:54 +0200

    update ssh key for siegi

--- nixos/system/users.nix
+++ nixos/system/users.nix
@@ -16,7 +16,7 @@
       ];
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM56oJGPCghkpixf0EET44ehH5K2VyCzf+NLfPSCxnq1 siegi@thymian"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1R2gEuXslK413gWBE4tOA894zO/MkhZrAK/LyRcsmo siegi@thymian"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdANrCkeXTrZha/w3pvg/vCZWmuRsy7cI6PmgVfWH8c siegi@blinkybill"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4/dxajByIB9PaameTee+vbKuWtE6/XJ3vg4JjwfBpl siegi@oz"
       ];
