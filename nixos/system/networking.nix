abb419a098327bde3aef03d9be5304b5a5e09e7c
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-04-19 12:16:19 +0200

    add vpn tunnel to monitoring server

--- nixos/system/networking.nix
+++ nixos/system/networking.nix
@@ -1,9 +1,34 @@
+{ config, ... }:
 {
   networking = {
     hostName = "ares";
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 81 443 ];
+      allowedTCPPorts = [
+        22
+        80
+        81
+        443
+      ];
     };
+  };
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "10.100.0.2/24" ];
+
+    privateKeyFile = config.sops.secrets."wireguard/private_key".path;
+
+    peers = [
+      {
+        publicKey = "8L3Cwpj1cRznCJ8zzV9//6EWQ20NCGAQWAFUO4bK4h8=";
+        allowedIPs = [ "10.100.0.1/32" ];
+        endpoint = "siegi.internet-box.ch:51820";
+        persistentKeepalive = 25;
+      }
+    ];
+  };
+  sops.secrets."wireguard/private_key" = {
+    mode = "640";
+    owner = "systemd-network";
+    group = "systemd-network";
   };
 }
