e9706fe1b4566cab04ca309d411831cc6d1df90f
Olivier Bitter <olibitter@gmail.com>
2026-04-24 20:22:08 +0200

    refactor ares module

--- modules/hosts/ares/ares.nix
+++ modules/hosts/ares/ares.nix
@@ -1,26 +1,27 @@
-{ inputs, ... }:
+{ inputs, self, ... }:
+let
+  hostname = "ares";
+in
 {
   systems = [
     "x86_64-linux"
     "aarch64-linux"
   ];
-
-  flake.nixosConfigurations =
-    let
-      hostname = "ares";
-    in
-    {
-      ${hostname} = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = with inputs.self.modules.nixos; [
-          ./_hardware-configuration.nix
-          siegi
-          zenoli
-          {
-            system.stateVersion = "25.11";
-            networking.hostName = hostname;
-          }
-        ];
-      };
+  flake.modules.nixos.${hostname} = {
+    imports = with self.modules.nixos; [
+      ./_hardware-configuration.nix
+      siegi
+      zenoli
+      {
+        system.stateVersion = "25.11";
+        networking.hostName = hostname;
+      }
+    ];
+  };
+  flake.nixosConfigurations = {
+    ${hostname} = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = with self.modules.nixos; [ ares ];
     };
+  };
 }
